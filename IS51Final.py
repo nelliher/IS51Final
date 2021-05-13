578613c3d3398ad52b8d076945610ff7c11d7db4
nelli her <nellih1117@gmail.com>
2021-05-13 16:13:40 -0700

    finish structured english

---
+++ IS51Final.py
@@ -0,0 +1,14 @@
+"""
+This program will display student grades on a final exam. The first option will display the number of grades. 
+The second option will display the average grade. And the third option will display the percentage of grades 
+that are above average. There will be three functions total. The first function will initialize the application. 
+The second function will calculate the percentage above average. The third function will calculate the percentage 
+of grades above average.
+
+Funtion 1 will initialize the application
+Function 2 will sum all the grades/ number of grades
+Function 3 will sum all grades above average.
+
+It will output the percentage above average. After the code has ran, it will output the percentage of grades that
+are above average.
+"""
